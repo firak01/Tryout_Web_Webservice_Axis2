23834c4883b8c98a60f35dd3c2eb8e74e5edf586
firak01 <firak01@t-online.de>
2017-06-01 11:34:32 +0200

    Erweiterung des WebService testen

--- Tryout.Web.Webservice.Axis2.HelloAxis2.001/src/tryout/web/webservice/axis2/helloaxis/HelloService.java
+++ Tryout.Web.Webservice.Axis2.HelloAxis2.001/src/tryout/web/webservice/axis2/helloaxis/HelloService.java
@@ -1,7 +1,17 @@
 package tryout.web.webservice.axis2.helloaxis;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class HelloService {
 	public String sayHello(String name){
 		return "Hello " + name;
+	}
+	public String getNow(){
+		Calendar cal = Calendar.getInstance();
+		//Date date = new Date();
+		Date date = cal.getTime();
+		String sReturn = new Integer(date.getYear()).toString() + new Integer(date.getMonth()).toString() + new Integer(date.getDay()).toString();
+		return sReturn;
 	}
 }
